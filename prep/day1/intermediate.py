db3c2967258608cbb9bf389e17e82d8693dc764a
EmilRR24 <grimlei2005@yahoo.com>
2022-04-04 16:23:02 -1000

    completed intermediate assignment

--- prep/day1/intermediate.py
+++ prep/day1/intermediate.py
@@ -21,3 +21,72 @@ print(sports_directory)
 # Change the value 20 in z to 30
 z[0]['y']= 30
 print(z)
+
+# Create a function iterateDictionary(some_list) that, given a list of dictionaries, the function loops through each dictionary in the list and prints each key and the associated value. For example, given the following list:
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionarycopy(some_list):
+    stringReturn = ''
+    for val in some_list:
+        stringReturn += f"first_name - {val['first_name']}, last_name - {val['last_name']}\n"
+    return stringReturn
+print(iterateDictionarycopy(students))
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+# Create a function iterateDictionary2(key_name, some_list) that, given a list of dictionaries and a key name, the function prints the value stored in that key for each dictionary. For example, iterateDictionary2('first_name', students) should output:
+
+def iterateFirstName(key_name, some_list):
+    firstName = ''
+    for val in some_list:
+        firstName += f"{val['first_name']}\n"
+    return firstName
+print(iterateFirstName('first_name', students))
+def iterateLastName(key_name, some_list):
+    lastName = ''
+    for val in some_list:
+        lastName += f"{val['last_name']}\n"
+    return lastName
+print(iterateLastName('last_name', students))
+
+# Create a function printInfo(some_dict) that given a dictionary whose values are all lists, prints the name of each key along with the size of its list, and then prints the associated values within each key's list. For example:
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(some_dict):
+    for key in some_dict:
+        print(f"{len(some_dict[key])} {key.upper()}")
+        for val in some_dict[key]:
+            print(val)
+        print('')
+printInfo(dojo)
+
+# output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
