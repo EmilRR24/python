3d91104ac3c4b0c38a7cdad89330b4715c7b7ea7
EmilRR24 <grimlei2005@yahoo.com>
2022-04-07 11:08:37 -1000

    completed playground

--- flask/fundamentals/playground/server.py
+++ flask/fundamentals/playground/server.py
@@ -1,11 +1,18 @@
 # install flask to directory everytime (hello_flask)
-from flask import Flask  # Import Flask to allow us to create our app
+from flask import Flask, render_template  # Import Flask to allow us to create our app
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
-def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
 #ADD CODE HERE
 
+@app.route('/play')          # The "@" decorator associates this route with the function immediately following
+def index():
+    return render_template("index.html", phrase='', times=5, color="lightblue")
+
+@app.route('/play/<int:x>')          # The "@" decorator associates this route with the function immediately following
+def index1(x):
+    return render_template("index.html", phrase='', times=x, color="lightblue")
+@app.route('/play/<int:x>/<color>')          
+def index2(x, color):
+    return render_template("index.html", phrase='', times=x, color=color)
 
 
     # app.run(debug=True) should be the very last statement! 
