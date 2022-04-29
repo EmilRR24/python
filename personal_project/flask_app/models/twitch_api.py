1e9529c74ef1ae8a42f8cd186599a0bb12b5cf12
EmilRR24 <grimlei2005@yahoo.com>
2022-04-29 11:40:21 -1000

    gitignore

--- personal_project/flask_app/models/twitch_api.py
+++ personal_project/flask_app/models/twitch_api.py
@@ -1,7 +1,7 @@
 import requests
 def get_twitch():
     url = 'https://api.twitch.tv/helix/games/top'
-    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8',}
+    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8', 'Authorization' : 'Bearer 3qbgrg28kobkxs1f184eq8cnmm9fgy', 'Client-Id' : '8b6o67lgkip4190y7u7poqqwno1eme'}
     r = requests.get(url, headers=headers)
     # print(r.json()['data'])
     return r.json()
