77d7af8b3dfa9f6486b81859bd7965cc9a5fc850
Emil Ramirez <101108019+EmilRR24@users.noreply.github.com>
2022-05-01 12:17:40 -1000

    Delete twitch_api.py

--- personal_project/flask_app/models/twitch_api.py
+++
@@ -1,6 +0,0 @@
-import requests
-def get_twitch():
-    url = 'https://api.twitch.tv/helix/games/top'
-    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8', 'Authorization' : 'Bearer 3qbgrg28kobkxs1f184eq8cnmm9fgy', 'Client-Id' : '8b6o67lgkip4190y7u7poqqwno1eme'}
-    r = requests.get(url, headers=headers)
-    return r.json()
